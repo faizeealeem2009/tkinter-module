4c02ac66e920a7ee35a9111a01fb718cc33cbcae
Faizee Abdul Aleem <faizeealeem9490@gmail.com>
2026-07-28 06:56:19 +0530

    File is modified

--- Lesson3regestration.py
+++ Lesson3regestration.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+def submit_data():
+    pass
+
+
 root=Tk()
 root.title("Student Regestration Form")
 #root.geometry("600x400")
@@ -50,5 +54,31 @@ R1.grid(row=7,column=0)
 R2=Radiobutton(root,value="Female",textvariable=gender)
 R2.grid(row=7,column=1)
 
+L9=Label(root,text="Select Hobbies:",font=("Calibri",16,"bold"))
+L9.grid(row=8,column=0,columnspan=2)
+
+C1=Checkbutton(root,text="Cricket",font=("Calibri",14,"bold"))
+C1.grid(row=9,column=0)
+
+C2=Checkbutton(root,text="Football",font=("Calibri",14,"bold"))
+C2.grid(row=10,column=0)
+
+C3=Checkbutton(root,text="Chess",font=("Calibri",14,"bold"))
+C3.grid(row=11,column=0)
+
+L10=Label(root,text="Type password: ",font=("Calibri",16,"bold"))
+L10.grid(row=12,column=0,columnspan=2)
+
+E5=Entry(root,font=("Calibri",14,"normal"),show="*")
+E5.grid(row=13,column=0,padx=10,pady=10,columnspan=2)
+
+L11=Label(root,text="Verify password: ",font=("Calibri",16,"bold"))
+L11.grid(row=14,column=0,columnspan=2)
+
+E6=Entry(root,font=("Calibri",14,"normal"),show="*")
+E6.grid(row=15,column=0,padx=10,pady=10,columnspan=2)
+
+B1=Button(root,text="Submit",command=submit_data,font=("Calibri",14,"bold"),padx=50,pady=5)
+B1.grid(row=16,column=0,padx=10,pady=10,columnspan=2)
 
 root.mainloop()
