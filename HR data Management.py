952d8fb76085323dee372e8ad20333b7080f254b
Faizee Abdul Aleem <faizeealeem9490@gmail.com>
2026-08-02 08:23:07 +0530

    file modified.

--- HR data Management.py
+++ HR data Management.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import csv
 from tkinter import messagebox
 with open("employees.csv","w",newline="") as f1:
@@ -15,10 +16,22 @@ def submit_data():
     exp=experience.get()
     jb=job.get()
     dp=dep.get()
-    sk=skill.get()
+    skill=""
+    if skill1.get():
+        skill+="Python "
+    else:
+        pass
+    if skill2.get():
+        skill+="Excel "
+    else:
+        pass
+    if skill3.get():
+        skill+="Graphic Designing "
+    else:
+        pass
     bs=basic.get()
-
-    empdata=[fname,lname,ct,gd,ed,exp,jb,dp,sk,bs]
+    
+    empdata=[fname,lname,ct,gd,ed,exp,jb,dp,skill,bs]
     with open("employees.csv","a",newline="") as f1:
             csvWriter=csv.writer(f1)
             csvWriter.writerow(empdata)
@@ -27,7 +40,6 @@ root=Tk()
 root.title("HR Data Management")
 #root.geometry("600x400")
 root.resizable(False,False)
-
 
 firstname=StringVar()
 lastname=StringVar()
@@ -39,6 +51,19 @@ job=StringVar()
 dep=StringVar()
 skill=StringVar()
 basic=IntVar()
+
+experience=StringVar()
+selected=list(range(16))
+experience.set(selected[2])
+
+
+sel_educ=["HSC","B.Com","B.Voc","BCA","B.Sc","M.Sc","B.E","MCA"]
+education.set(sel_educ[2])
+
+skill1=BooleanVar()
+skill2=BooleanVar()
+skill3=BooleanVar()
+
 L1=Label(root,text="HR Data Management",font=("Calibri",20,"bold"))
 L1.grid(row=0,column=0,columnspan=2,padx=10,pady=10)
 
@@ -72,23 +97,23 @@ R2.grid(row=5,column=1)
 L9=Label(root,text="City:",font=("Calibri",14,"normal"))
 L9.grid(row=6,column=0,padx=10,pady=10)
 
-L10=Label(root,text="Education:",font=("Calibri",14,"normal"))
-L10.grid(row=6,column=1,padx=10,pady=10)
+L10=Label(root,text="Select Education:",font=("Calibri",14,"normal"))
+L10.grid(row=6,column=1)
 
 E3=Entry(root,font=("Calibri",14,"normal"),textvariable=city)
 E3.grid(row=7,column=0,padx=10,pady=10)
 
-E4=Entry(root,font=("Calibri",14,"normal"),textvariable=education)
-E4.grid(row=7,column=1,padx=10,pady=10)
+E4=ttk.Combobox(root,font=("Calibri",14,"normal"),textvariable=education,values=sel_educ)
+E4.grid(row=7,column=1)
 
-L11=Label(root,text="Experience:",font=("Calibri",14,"normal"))
-L11.grid(row=8,column=0,padx=10,pady=10)
+L11=Label(root,text="Select Experience:",font=("Calibri",14,"normal"))
+L11.grid(row=8,column=0)
 
 L12=Label(root,text="Job Profile:",font=("Calibri",14,"normal"))
 L12.grid(row=8,column=1,padx=10,pady=10)
 
-E5=Entry(root,font=("Calibri",14,"normal"),textvariable=experience)
-E5.grid(row=9,column=0,padx=10,pady=10)
+E5=ttk.Combobox(root,font=("Calibri",14,"normal"),textvariable=experience,values=selected)
+E5.grid(row=9,column=0)
 
 E6=Entry(root,font=("Calibri",14,"normal"),textvariable=job)
 E6.grid(row=9,column=1,padx=10,pady=10)
@@ -96,20 +121,28 @@ E6.grid(row=9,column=1,padx=10,pady=10)
 L11=Label(root,text="Department:",font=("Calibri",14,"normal"))
 L11.grid(row=10,column=0,padx=10,pady=10)
 
-L12=Label(root,text="Skills:",font=("Calibri",14,"normal"))
+L12=Label(root,text="Enter Basic Salary:",font=("Calibri",14,"normal"))
 L12.grid(row=10,column=1,padx=10,pady=10)
 
 E7=Entry(root,font=("Calibri",14,"normal"),textvariable=dep)
 E7.grid(row=11,column=0,padx=10,pady=10)
 
-E8=Entry(root,font=("Calibri",14,"normal"),textvariable=skill)
+E8=Entry(root,font=("Calibri",14,"normal"),textvariable=basic)
 E8.grid(row=11,column=1,padx=10,pady=10)
 
-L13=Label(root,text="Enter Basic Salary:",font=("Calibri",14,"normal"))
-L13.grid(row=12,column=0,padx=10,pady=10)
+L13=Label(root,text="Select Skills:",font=("Calibri",14,"bold"))
+L13.grid(row=12,column=0,columnspan=2)
 
-E9=Entry(root,font=("Calibri",14,"normal"),textvariable=basic)
-E9.grid(row=13,column=0,padx=10,pady=10)
+cb1=Checkbutton(root,text="Python",font=("Calibri",14,"bold"),variable=skill1)
+cb1.grid(row=13,column=0)
+
+cb2=Checkbutton(root,text="Excel",font=("Calibri",14,"bold"),variable=skill2)
+cb2.grid(row=13,column=1)
+
+cb3=Checkbutton(root,text="Graphic Designing",font=("Calibri",14,"bold"),variable=skill3)
+cb3.grid(row=13,column=2)
+
+
 
 B1=Button(root,text="Submit",command=submit_data,font=("Calibri",14,"bold"),bg="green",padx=50,pady=5)
 B1.grid(row=14,column=0,padx=10,pady=10,columnspan=2)
